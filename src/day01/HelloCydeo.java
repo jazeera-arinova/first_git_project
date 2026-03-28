0b76e69977e3a9d6d84286ba265968ca5da14ad2
Zhazira (Jazeera) Arinova <137842672+jazeera-arinova@users.noreply.github.com>
2026-03-28 12:42:12 -0400

    Add more greeting messages in HelloCydeo
    Added additional greeting messages to the output.

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -4,5 +4,9 @@ public class HelloCydeo {
     public static void main(String[] args) {
 
         System.out.println("HelloCydeo! ");
+        System.out.println("HelloCydeo Friends! ");
+        System.out.println("HelloCydeo Ladies! ");
+        System.out.println("HelloCydeo babies! ");
+
     }
 }
